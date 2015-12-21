59098f92ae20e732610ef5cdd8fe2bb5bb204188
Alex Koo <akoo@apigee.com>
2015-12-21 09:29:54 -0800

    add test URLs

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 apigeetool deploynodeapp -u $ae_username -p $ae_password -o $org -e $env -n 'diego-nock-mock' -d . -m app.js -b /diego-nock-mock -R
+echo "You can now test out the API proxy by entering http://$org-$env.apigee.net/diego-nock-mock in cURL or your browser"
